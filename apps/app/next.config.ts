82d0f8de005968231d2be966e2cda45a263209e9
absolutegravitas <124488796+absolutegravitas@users.noreply.github.com>
2025-07-05 17:07:53 +1000

    rem toolbar & collab

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,16 +1,19 @@
-import { env } from '@/env';
-import { withToolbar } from '@repo/feature-flags/lib/toolbar';
-import { config, withAnalyzer } from '@repo/next-config';
-import { withLogging, withSentry } from '@repo/observability/next-config';
-import type { NextConfig } from 'next';
+import { env } from "@/env";
+// import { withToolbar } from '@repo/feature-flags/lib/toolbar';
+import { config, withAnalyzer } from "@repo/next-config";
+import { withLogging, withSentry } from "@repo/observability/next-config";
+import type { NextConfig } from "next";
 
-let nextConfig: NextConfig = withToolbar(withLogging(config));
+let nextConfig: NextConfig =
+  //  withToolbar(
+  withLogging(config);
+// );
 
 if (env.VERCEL) {
   nextConfig = withSentry(nextConfig);
 }
 
-if (env.ANALYZE === 'true') {
+if (env.ANALYZE === "true") {
   nextConfig = withAnalyzer(nextConfig);
 }
 
